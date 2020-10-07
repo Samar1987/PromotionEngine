cbc12f169173956023432d984997a5057f94e5be
Sammaar <samar.rajput15@gmail.com>
2020-10-07 21:31:46 +0530

    Added SKU Ids to container class SKUID.cs

--- PromotionEngine/Entities/SKUID.cs
+++ PromotionEngine/Entities/SKUID.cs
@@ -6,6 +6,8 @@ namespace PromotionEngine.Entities
 {
     public static class SKUID
     {
+        public static string[] SKUIDs = new string[] { "A", "B", "C", "D" };
+
         public static decimal APromotionPrice = 130m;
         public static decimal BPromotionPrice = 45m;
         public static decimal CDPromotionPrice = 30m;
