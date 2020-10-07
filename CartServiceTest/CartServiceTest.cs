942dbc29407d8928b6a054b9555d9cb6d10db9fa
Sammaar <samar.rajput15@gmail.com>
2020-10-07 19:10:57 +0530

    Added test methods for all 3 promotion type scenarios.

--- CartServiceTest/CartServiceTest.cs
+++ CartServiceTest/CartServiceTest.cs
@@ -6,8 +6,127 @@ namespace CartServiceTest
     public class CartServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetWholeOrderTotalPrice_WithScenario_A_ValidOrderValue()
         {
+            // [ Scenario A ] =>
+            // 1 * A 50
+            // 1 * B 30
+            // 1 * C 20
+            // [ Total Order Value is: 100 ]
+
+            // Arrange
+            double expected = 100;
+            ICartService _cartService = new CartService();
+            Product prod;
+
+            // Adding 1 A Type Product.
+            prod = new Product();
+            prod.Id = "A";
+            _cartService.AddProduct(prod);
+
+            // Adding 1 B Type Product.
+            prod = new Product();
+            prod.Id = "B";
+            _cartService.AddProduct(prod);
+
+            // Adding 1 C Type Product.
+            prod = new Product();
+            prod.Id = "C";
+            _cartService.AddProduct(prod);
+
+            // Act
+            decimal actual = _cartService.GetWholeOrderTotalPrice();
+
+            // Assert 
+            Assert.AreEqual(expected, Convert.ToDouble(actual), 0.001, "Total Order Value is valid.");
+
+        }
+
+        [TestMethod]
+        public void GetWholeOrderTotalPrice_WithScenario_B_ValidOrderValue()
+        {
+            // [ Scenario B ] =>
+            // 5 * A 130 + 2 * 50
+            // 5 * B 45 + 45 + 30
+            // 1 * C 20
+            // [ Total Order Value is: 370 ]
+
+            // Arrange
+            double expected = 370;
+            ICartService _cartService = new CartService();
+            Product prod;
+
+            // Adding 5 A Type Product.
+            for (int i = 0; i < 5; i++)
+            {
+                prod = new Product();
+                prod.Id = "A";
+                _cartService.AddProduct(prod);
+            }
+
+            // Adding 5 B Type Product.
+            for (int i = 0; i < 5; i++)
+            {
+                prod = new Product();
+                prod.Id = "B";
+                _cartService.AddProduct(prod);
+            }
+
+            // Adding 1 C Type Product.
+            prod = new Product();
+            prod.Id = "C";
+            _cartService.AddProduct(prod);
+
+            // Act                        
+            decimal actual = _cartService.GetWholeOrderTotalPrice();
+
+            // Assert 
+            Assert.AreEqual(expected, Convert.ToDouble(actual), 0.001, "Total Order Value is valid.");
+
+        }
+
+        [TestMethod]
+        public void GetWholeOrderTotalPrice_WithScenario_C_ValidOrderValue()
+        {
+            // [Scenario C] =>
+            // 3 * A 130
+            // 5 * B 45 + 45 + 1 * 30
+            // 1 * C -
+            // 1 * D 15
+            // Total Order Value is: 265
+
+            // Arrange
+            double expected = 265;
+            ICartService _cartService = new CartService();
+            Product prod;
+
+            // Adding 3 A Type Product.
+            for (int i = 0; i < 3; i++)
+            {
+                prod = new Product();
+                prod.Id = "A";
+                _cartService.AddProduct(prod);
+            }
+
+            // Adding 5 B Type Product.
+            for (int i = 0; i < 5; i++)
+            {
+                prod = new Product();
+                prod.Id = "B";
+                _cartService.AddProduct(prod);
+            }
+
+            // Adding 1 D Type Product.
+            prod = new Product();
+            prod.Id = "D";
+            _cartService.AddProduct(prod);
+
+            // Act                        
+            decimal actual = _cartService.GetWholeOrderTotalPrice();
+
+            // Assert 
+            Assert.AreEqual(expected, Convert.ToDouble(actual), 0.001, "Total Order Value is valid.");
+
         }
     }
 }
