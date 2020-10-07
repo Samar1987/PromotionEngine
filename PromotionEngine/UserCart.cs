32ec420f76870cd4d8bd01ebb13d84bb7126c1c6
Sammaar <samar.rajput15@gmail.com>
2020-10-07 22:06:28 +0530

    Leting the end user access services like adding products to order list and then get total order value when 2 promotion types meets.

--- PromotionEngine/UserCart.cs
+++ PromotionEngine/UserCart.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using PromotionEngine.Entities;
+using PromotionEngine.Service;
+using System;
 
 namespace PromotionEngine
 {
+    // User interface (consumer class) need to be interacted by end user to avail services from Promotion Engine app's ICartService. 
     public class UserCart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Total number of order(s)");
+            int totalOreder = Convert.ToInt32(Console.ReadLine());
+
+
+            // [Interface Repository pattern]
+            ICartService _cart = new CartService();
+
+            for (int i = 0; i < totalOreder; i++)
+            {
+                Console.WriteLine("Enter the product type");
+                string productType = Console.ReadLine();
+               
+                Product prod = new Product { Id = productType };
+                _cart.AddProduct(prod);
+            }
+
+
+            decimal totalPrice = _cart.GetWholeOrderTotalPrice();
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Your Total Order Value: {0}", totalPrice);
+            Console.ReadLine();
         }
     }
 }
