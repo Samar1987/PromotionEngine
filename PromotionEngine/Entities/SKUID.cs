f7974b7c91aa5eaee1952619a334dc5ed16d9f21
Sammaar <samar.rajput15@gmail.com>
2020-10-07 21:28:51 +0530

    Added SKUID container class that contains the promotional prices for 3 promotion types.

---
+++ PromotionEngine/Entities/SKUID.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Entities
+{
+    public static class SKUID
+    {
+        public static decimal APromotionPrice = 130m;
+        public static decimal BPromotionPrice = 45m;
+        public static decimal CDPromotionPrice = 30m;
+    }
+}
