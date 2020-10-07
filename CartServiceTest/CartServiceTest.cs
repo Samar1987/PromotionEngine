f32edebfe59649ccebc8f55ae580ff86efb374de
Sammaar <samar.rajput15@gmail.com>
2020-10-07 18:39:00 +0530

    Add project files.

---
+++ CartServiceTest/CartServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartServiceTest
+{
+    [TestClass]
+    public class CartServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PromotionEngine/CartService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionEngine
+{
+    public class CartService
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
