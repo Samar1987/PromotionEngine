9ef42039f5451b500fc06b4271634f83de952e74
Samar1987 <53080641+Samar1987@users.noreply.github.com>
2020-10-09 13:06:07 +0530

    Added comment line for business method GetTotalPrice()

--- PromotionEngine/Products/DProductPack.cs
+++ PromotionEngine/Products/DProductPack.cs
@@ -6,6 +6,7 @@ namespace PromotionEngine.Products
         public int ProductCount { get; set; }
         public decimal ProductPrice { get; set; }
 
+        // This method [logic] can be changed for this product in future as per the business requirment.        
         public decimal GetTotalPrice() => ProductCount * ProductPrice;
     }
 }
